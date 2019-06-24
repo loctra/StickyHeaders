c3e75970242e530bcf183f7bd29379e35eaa1d26
Loctra <loc.tra@tomosia.com>
2019-06-24 13:52:01 +0700

    add sticky header

--- app/src/main/java/com/lt/stickyheaders/MainActivity.java
+++ app/src/main/java/com/lt/stickyheaders/MainActivity.java
@@ -3,6 +3,8 @@ package com.lt.stickyheaders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.lt.stickyheader.StickyRecyclerHeadersDecoration;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
